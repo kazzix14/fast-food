57e91fcd42d2a2efe34ac57917c474ec5c2c1490
kazzix <kazzix14@gmail.com>
2024-02-26 16:21:51 +0900

    wip

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# fastfood
+# fast-food
 
-Fastfood is a CLI tool designed to make your command line experience as easy and addictive as fast food. It allows you to use commands with fewer types, liberating you from the need to remember the syntax for each command. With `fastfood`, you can create shortcuts for your most used commands and execute them with minimal input, just like using the `ff` shortcut.
+Fastfood is a CLI tool designed to make your command line experience as easy and addictive as fast food. It allows you to use commands with fewer types, liberating you from the need to remember the syntax for each command. With `fast-food`, you can create shortcuts for your most used commands and execute them with minimal input, just like using the `ff` shortcut.
 
 ## Features
 
@@ -18,24 +18,24 @@ Before you begin, ensure you have the following installed:
 
 ### Installation
 
-1. Clone the `fastfood` repository:
+1. Clone the `fast-food` repository:
 ```sh
-git clone https://github.com/yourusername/fastfood.git
+git clone https://github.com/yourusername/fast-food.git
 ```
 
-2. Change into the `fastfood` directory:
+2. Change into the `fast-food` directory:
 ```sh
-cd fastfood
+cd fast-food
 ```
 
-3. Install `fastfood` using the setup script:
+3. Install `fast-food` using the setup script:
 ```sh
 ./setup install
 ```
 
 ### Configuration
 
-To configure your shortcuts, edit the `config.json` file in the `fastfood` directory. Here's an example configuration:
+To configure your shortcuts, edit the `config.json` file in the `fast-food` directory. Here's an example configuration:
 
 ```json
 {
@@ -59,7 +59,7 @@ This will execute the `ls` command using the shortcut defined in your `config.js
 
 ## Contributing
 
-Contributions are welcome! If you have a suggestion that would make `fastfood` better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+Contributions are welcome! If you have a suggestion that would make `fast-food` better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
 
 ## License
 
@@ -68,5 +68,5 @@ Distributed under the MIT License. See `LICENSE` for more information.
 ## Acknowledgments
 
 - Inspired by the convenience of fast food and the desire to streamline command-line operations.
-- Thanks to all contributors who help make `fastfood` better.
+- Thanks to all contributors who help make `fast-food` better.
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,76 @@
+use clap::{ArgMatches, Command};
+use serde::{Deserialize, Serialize};
+use serde_yaml;
+use std::fs;
+use std::process::Command as ProcessCommand;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct CommandConfig {
+    name: String,
+    command: Option<String>,
+    subs: Option<Vec<CommandConfig>>,
+}
+
+fn build_command(cmd_config: CommandConfig) -> Command {
+    let mut cmd =
+        Command::new(cmd_config.name.clone()).about(cmd_config.command.clone().unwrap_or_default());
+
+    if let Some(ref subs) = cmd_config.subs {
+        for sub in subs {
+            let sub_cmd = build_command(sub.clone());
+            cmd = cmd.subcommand(sub_cmd);
+        }
+    }
+
+    cmd
+}
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = fs::read_to_string("config.yaml").expect("Failed to read config.yaml");
+
+    // Directly deserialize YAML into a Vec<CommandConfig>
+    let config: Vec<CommandConfig> = serde_yaml::from_str(&yaml).expect("Failed to parse YAML");
+
+    let mut app = Command::new("fastfood");
+
+    for cmd_config in config.clone() {
+        let cmd = build_command(cmd_config);
+        app = app.subcommand(cmd);
+    }
+
+    let matches = app.get_matches();
+
+    execute_command(matches, &config);
+}
+
+fn execute_command(matches: ArgMatches, config: &Vec<CommandConfig>) {
+    if let Some(name) = matches.subcommand_name() {
+        for cmd_config in config {
+            //if let Some(subs) = &cmd_config.subs {
+            //    dbg!(&name);
+            //    execute_command(name, subs);
+            //}
+            if cmd_config.name == name {
+                // Split the command from the arguments
+                if let Some(command) = cmd_config.command.clone() {
+                    println!("Executing command: {}", command);
+                    let parts: Vec<&str> = command.split_whitespace().collect();
+                    let (command, args) = parts.split_first().unwrap();
+
+                    // Execute the command
+                    let output = ProcessCommand::new(command)
+                        .args(args)
+                        .output()
+                        .expect("Failed to execute command");
+
+                    // Print the output
+                    println!("Output: {}", String::from_utf8_lossy(&output.stdout));
+                }
+                if let Some(matches) = matches.subcommand_matches(name) {
+                    execute_command(matches.clone(), config);
+                }
+                break;
+            }
+        }
+    }
 }
